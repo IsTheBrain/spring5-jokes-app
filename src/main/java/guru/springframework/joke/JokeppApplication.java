c9ba306fc4124ba4d592005c2a16a7c437bcd10e
Michele Brini <michele.brini@finconsgroup.local>
2020-04-20 13:04:18 +0200

    Updated some files

--- src/main/java/guru/springframework/joke/JokeppApplication.java
+++ src/main/java/guru/springframework/joke/JokeppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5JokesAppApplication {
+public class JokeppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5JokesAppApplication.class, args);
+		SpringApplication.run(JokeppApplication.class, args);
 	}
 
 }
--- src/test/java/guru/springframework/joke/JokeppApplicationTests.java
+++ src/test/java/guru/springframework/joke/JokeppApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Spring5JokesAppApplicationTests {
+class JokeppApplicationTests {
 
 	@Test
 	void contextLoads() {
